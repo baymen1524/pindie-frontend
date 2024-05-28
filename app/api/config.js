0d3f2f6191f4fc2fd7cf4b1c8df4c22e5f423ae8
baymen1524 <baymen1524@gmail.com>
2024-05-28 14:24:16 +0300

    <сообщение коммита>

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'http://frontpindieeeee.nomoredomainswork.ru'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/users/me`,
+}
